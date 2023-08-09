13611acfd2ce4ac7ad7d71366fb40dd33cdf0d0c
HoangSon <son.lyhoang2014@gmail.com>
2023-08-09 15:53:56 +0700

    Footer2

--- src/layouts/component/Footer/index.js
+++ src/layouts/component/Footer/index.js
@@ -12,27 +12,26 @@ function Footer() {
                 // as="nav"
                 display={'flex'}
                 align="center"
-                justify={{base:'center',md:"space-between"}}
+                justify={{ base: 'center', md: 'space-between' }}
                 wrap="wrap"
-             
-                
-                bg="gray.50"
                 marginX={{ base: '', md: '54px' }}
             >
                 <Grid
-                    bg="gray"
-                    templateColumns={{sm:'repeat(1,1fr)' ,md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+                    bg="green.800"
+                    templateColumns={{ sm: 'repeat(1,1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
                     gap={[4, 6]}
-                    paddingY={{base:'4rem',md:'0px'}}
+                    paddingY={{ base: '4rem', md: '0px' }}
                     height={{ base: '', md: '159px' }}
                     alignItems={'center'}
                     padding="5rem"
+               
                 >
                     <GridItem
+                        color="gray.50"
                         fontSize={{ base: '2xl', md: '4xl' }}
                         fontWeight="bold"
                         display={'flex'}
-                        justifyContent={{base:'center',md:'start'}}
+                        justifyContent={{ base: 'center', md: 'start' }}
                     >
                         PLANTNEST
                     </GridItem>
@@ -40,32 +39,36 @@ function Footer() {
                     <GridItem
                         display={'flex'}
                         justifyContent={'center'}
-                        flexDirection={{base:'column', md:'row'}}
+                        flexDirection={{ base: 'column', md: 'row' }}
                         justify={'center'}
                         alignItems={{ base: 'center', md: 'center', lg: 'flex-start' }}
                         fontSize={{ base: '16px', sm: '16px', md: '18px', lg: '2xl', xl: '2xl' }}
+                        color="gray.50"
                         gap={6}
                     >
-                        <Box>
-                            <NavLink to="/">Help</NavLink>
+                        <Box _hover={{fontWeight:'bold'}} cursor={'pointer'}>
+                            <NavLink to="/" >Home</NavLink>
                         </Box>
-                        <Box>
-                            <NavLink to="/">Contact us</NavLink>
+                        <Box _hover={{fontWeight:'bold'}} cursor={'pointer'}>
+                            <NavLink to="/" >About US</NavLink>
                         </Box>
-                        <Box>
-                            <NavLink to="/">Privacy & Terms</NavLink>
+                        <Box  _hover={{fontWeight:'bold'}} cursor={'pointer'}>
+                            <NavLink to="/">Feed Back</NavLink>
+                        </Box>
+                        <Box _hover={{fontWeight:'bold'}} cursor={'pointer'}>
+                            <NavLink to="/" >Contact US</NavLink>
                         </Box>
                     </GridItem>
 
-                    <GridItem display={'flex'} justifyContent={{base:'center', md:'end'}}>
-                        <Box px='4'>
-                            <ImTwitter fontSize={'2rem'}/>
+                    <GridItem display={'flex'} justifyContent={{ base: 'center', md: 'end' }}>
+                        <Box px={{base:"4",md:"2"}} color="gray.50">
+                            <ImTwitter color="gray.50" fontSize={'2rem'} />
                         </Box>
-                        <Box px='2'>
-                            <ImFacebook fontSize={'2rem'}/>
+                        <Box px={{base:"4",md:"2"}} color="gray.50">
+                            <ImFacebook fontSize={'2rem'} />
                         </Box>
-                        <Box px='2'>
-                            <ImInstagram fontSize={'2rem'}/>
+                        <Box px={{base:"4",md:"2"}} color="gray.50">
+                            <ImInstagram fontSize={'2rem'} />
                         </Box>
                     </GridItem>
                 </Grid>
