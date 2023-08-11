80dc9efe0a71f2d9b4e8d1d5bc3011068842686c
TUS9x <nguyentuld999@gmail.com>
2023-08-11 14:44:38 +0700

    123

--- src/page/PlantCatalog/index.js
+++ src/page/PlantCatalog/index.js
@@ -14,8 +14,8 @@ function PlantCatalog() {
             <AccordionItem>
                 <h2>
                 <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Flowering
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'>
+                    FLOWER
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
@@ -32,8 +32,8 @@ function PlantCatalog() {
             <AccordionItem>
                 <h2>
                 <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Non-Flowering
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'> 
+                    NON-FLOWERING
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
@@ -50,24 +50,8 @@ function PlantCatalog() {
             <AccordionItem>
                 <h2>
                 <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Indoor
-                    </Box>
-                    <AccordionIcon />
-                </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
-                </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-                <h2>
-                <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Outdoor
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'>
+                    INDOOR
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
@@ -84,8 +68,8 @@ function PlantCatalog() {
             <AccordionItem>
                 <h2>
                 <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Succulents
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'>
+                    OUTDOOR
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
@@ -102,8 +86,26 @@ function PlantCatalog() {
             <AccordionItem>
                 <h2>
                 <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                    Medicinal
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'>
+                    SUCCULENTS
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                <Box display={{base:'block', sm:'grid'}} gridTemplateColumns='repeat(4,1fr)' gap={4} width='100%'>
+                    <Item/>
+                    <Item/>
+                    <Item/>
+                    <Item/>
+                </Box>
+                </AccordionPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <h2>
+                <AccordionButton>
+                    <Box as="span" flex='1' textAlign='center' fontWeight='bold'  color='green.800'>
+                    MEDICINAL
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
